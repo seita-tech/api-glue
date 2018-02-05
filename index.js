5b5143159e3a3843870f89cd45213132380bf6ff
Josmar Dias <josmarnet@gmail.com>
2018-02-05 02:34:25 -0200

    simplify code by using expand-json package

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
 const _ = require('lodash')
 const request = require('request')
-const deep = require('lodash-deep')
-const handlebars = require('handlebars')
+const expandJson = require('expand-json').expandJson
 
 const input = require('./input.json')
 const mapping = require('./mapping.json')
 
-const interpolateParam = (value, path) => {
-  if (!_.isString(value)) return value;
 
-  const template = handlebars.compile(value)
+const output = expandJson(mapping, input)
 
-  return template(input)
-
-}
-
-const mapper = (mapping) =>
-  deep.deepMapValues(mapping, interpolateParam);
-
-const output = mapper(mapping)
 output.json = _.isPlainObject(output.body)
+
 request(output)
